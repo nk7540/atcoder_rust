a68de6eeff8b2ce124e97c455f5c9de415c60f4f
nk7540 <ryo.prfm27@gmail.com>
2023-07-07 23:51:15 +0900

    abc077c

--- src/contest/beginners_selection/binary/abc077c.rs
+++ src/contest/beginners_selection/binary/abc077c.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 use proconio::input;
 #[allow(clippy::uninlined_format_args)]
 // Unsupported features in 1.42.0
@@ -5,5 +7,21 @@ use proconio::input;
 // the trait `std::iter::Iterator` is not implemented for `[({integer}, {integer}); 4]`
 
 fn main() {
-    input! {}
+    input! {
+        n: usize, // O(10^5)
+        mut a: [u32; n],
+        mut b: [u32; n],
+        mut c: [u32; n],
+    }
+    // naive: O(n^3)=O(10^15)
+    // sort: O(nlogn * 3)
+    // for each a[i], find partition in B, then in C: O(n(logn*2))
+    a.sort_unstable();
+    b.sort_unstable();
+    c.sort_unstable();
+    let mut ans = 0;
+    for ai in a {
+        // find partition in B
+        b.binary_search(&ai)
+    }
 }
